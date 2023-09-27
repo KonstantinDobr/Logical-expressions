6cba1aad75c49b1d77db783806c5230c894fd8b0
KonstantinDobr <snastink@yandex.ru>
2023-09-27 22:04:53 +0300

    Second task
    Implementation of the second task

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,17 @@ public class Main {
     public static void main(String[] args){
         // tasks
         task1();
+        task2();
     }
     public static void task1(){
         Scanner in = new Scanner(System.in);
         double x = in.nextDouble();
         System.out.println(3 <= x && x <= 8);
+    }
+
+    public static void task2(){
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        System.out.println(-3 <= x && x <= 5 || 9 <= x && x <= 15);
     }
 }
